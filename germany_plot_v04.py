f6a139a49f5a61364af64519303d08278f846ab7
BenjaminGolub <107389350+BenjaminGolub@users.noreply.github.com>
2024-08-26 09:22:24 +0200

    Update germany_plot_v04.py
    label location of "Mainz"

--- germany_plot_v04.py
+++ germany_plot_v04.py
@@ -148,7 +148,7 @@ while m <2 :
 				elif city[n]=="Bochum":
 					plt.annotate(city[n], xy=(x,y), xytext=(-4.0,-2.0), textcoords="offset points", ha="right", size="5")	
 				elif city[n]=="Mainz":
-					plt.annotate(city[n], xy=(x,y), xytext=(-2.0,-7.0), textcoords="offset points", ha="center", size="5")					
+					plt.annotate(city[n], xy=(x,y), xytext=(-4.0,-7.0), textcoords="offset points", ha="center", size="5")					
 				elif city[n]=="Potsdam":
 					plt.annotate(city[n], xy=(x,y), xytext=(4.0,-4.0), textcoords="offset points", size="5")	
 				#elif city[n]=="Hannover" or city[n]=="Halle" or city[n]=="Münster":
